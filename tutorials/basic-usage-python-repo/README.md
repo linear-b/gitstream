ab7f591a953f9e678ea9699dc5c92b8641d29a23
ofer affias <ofer.affias@gmail.com>
2022-12-13 12:48:53 +0200

    added tutotial files

---
+++ tutorials/basic-usage-python-repo/README.md
@@ -0,0 +1,2 @@
+# gitStreamDemo
+A demo showing how to set up gitStream on you first repo
---
+++ tutorials/basic-usage-python-repo/main.py
@@ -0,0 +1,8 @@
+def show_message(name1, name2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}. Welcome to the gitStream world')  # Press Ctrl+8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Mike')
