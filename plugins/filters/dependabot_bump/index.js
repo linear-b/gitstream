943fbcc418ad49dd656a2fa4f542aadaafa57252
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-12-14 16:44:08 -0500

    copilot recommendation

--- plugins/filters/dependabot_bump/index.js
+++ plugins/filters/dependabot_bump/index.js
@@ -1,13 +1,15 @@
 /**
- * Parse dependabot version bump, and extract the versions upgraded 
+ * Parse dependabot version bump and extract the versions upgraded.
  *
- * It assumes a pattern `from`` v1 `to`` v2.
- * It returns a list of these 2 items, first the new version and then the original.
+ * This function takes a description string as input and assumes a pattern of `from v1 to v2`.
+ * It extracts the new version (v2) and the original version (v1) from the description.
+ *
+ * @param {string} desc - The description string containing the version bump information.
+ * @returns {Array} - An array containing the new version (v2) at index 0 and the original version (v1) at index 1.
  */
-
 module.exports = (desc) => {
   var [_, from, to] = /Bumps.*from ([\d\.]+[A-Za-zαß]*) to ([\d\.]+[A-Za-zαß]*)/.exec(desc);
-  // remove trailing dot on to
+  // Remove trailing dot on the 'to' version
   if (to[to.length - 1] === ".") {
     to = to.slice(0, -1);
   }
