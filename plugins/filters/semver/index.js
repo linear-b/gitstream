e4c3de6d9bcb0a0dcbe0778c841ee6312263ef44
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-12-14 16:29:14 -0500

    Copilot recommendation

--- plugins/filters/semver/index.js
+++ plugins/filters/semver/index.js
@@ -1,22 +1,37 @@
 /**
- * Compares two software version numbers (e.g., "1.2.1" or "1.2b") and determines the type of version change.
+ * Compares two software version numbers and determines the type of version change.
  *
- * The first version to be compared, and the second are passed as argument 1 and 2 or as array of 2 items. 
- * When v1 > v2 the it means and upgrade.
+ * This is can be useful if you want to auto-approve specific types of Dependabot or Renovate updates, 
+ * or provide dependency update information to users
  * 
- * The options arument is optional and flags that affect comparison behavior:
- *   lexicographical: (true/[false]) compares each part of the version strings lexicographically instead of naturally; 
- *                    this allows suffixes such as "b" or "dev" but will cause "1.10" to be considered smaller than "1.2".
- *   zeroExtend: ([true]/false) changes the result if one version string has less parts than the other. In
- *               this case the shorter string will be padded with "zero" parts instead of being considered smaller.
+ * The first version to be compared is passed as v1, and the second version is passed as v2.
+ * If v1 > v2, it means an upgrade.
  *
- * It returns a string of either:
- *   'major' if the major version is incremented.
- *   'minor' if the minor version is incremented.
- *   'patch' if the patch version is incremented.
- *   'downgrade' if the second version is lower than the first.
- *   'equal' if both versions are equal.
- *   'error' if the comparison is abnormal or cannot be determined.
+ * The options argument is optional and can include the following flags:
+ *   - lexicographical: (true/[false]) compares each part of the version strings lexicographically instead of naturally.
+ *                      This allows suffixes such as "b" or "dev", but it will cause "1.10" to be considered smaller than "1.2".
+ *   - zeroExtend: ([true]/false) changes the result if one version string has fewer parts than the other.
+ *                 If set to true, the shorter string will be padded with "zero" parts instead of being considered smaller.
+ *
+ * The function returns a string indicating the type of version change:
+ *   - 'major' if the major version is incremented.
+ *   - 'minor' if the minor version is incremented.
+ *   - 'patch' if the patch version is incremented.
+ *   - 'downgrade' if the second version is lower than the first.
+ *   - 'equal' if both versions are equal.
+ *   - 'error' if the comparison is abnormal or cannot be determined.
+ *
+ * Examples:
+ *   - compareVersions("1.2.1", "1.2.2") returns 'patch'
+ *   - compareVersions("2.0.0", "1.9.0") returns 'major'
+ *   - compareVersions("1.2.3", "1.2.3") returns 'equal'
+ *
+ * @param {string} v1 - The first version to compare.
+ * @param {string} v2 - The second version to compare.
+ * @param {object} [options] - Optional flags that affect comparison behavior.
+ * @param {boolean} [options.lexicographical=false] - Whether to compare each part of the version strings lexicographically.
+ * @param {boolean} [options.zeroExtend=true] - Whether to pad the shorter version string with "zero" parts.
+ * @returns {string} - The type of version change.
  */
 
 module.exports = (v1, v2, options = {}) => {
