ebf596226cd7bf8bd0ad9d6550edad37b0eee39e
ofer affias <ofer.affias@gmail.com>
2023-12-26 10:15:15 +0200

    missing index js + snyk

---
+++ plugins/filters/snyk_bump/index.js
@@ -0,0 +1,15 @@
+module.exports = (desc) => {
+  if (desc && desc !== '""' && desc !== "''" ) {    
+    const matches = /Upgrade.*from ([\d\.]+[A-Za-zαß]*) to ([\d\.]+[A-Za-zαß]*)/.exec(desc);
+    if (matches && matches.length == 3) {
+      var [_, from, to] = matches;
+      // remove trailing dot on to
+      if (to[to.length - 1] === ".") {
+        to = to.slice(0, -1);
+      }
+      return [to, from];
+    }
+  }
+
+  return null;
+}
